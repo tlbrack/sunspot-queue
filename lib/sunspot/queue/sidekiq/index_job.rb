db8ccbfe6bbae46d0b37ec8b742a5ad02c058b0b
Travis Brack <tlbrack@zipments.com>
2016-01-21 10:16:34 -0500

    unscope and allow object to not exist without error

--- lib/sunspot/queue/sidekiq/index_job.rb
+++ lib/sunspot/queue/sidekiq/index_job.rb
@@ -9,7 +9,7 @@ module Sunspot::Queue::Sidekiq
 
     def perform(klass, id)
       without_proxy do
-        constantize(klass).find(id).solr_index
+        constantize(klass).unscoped.find_by_id(id).try(:solr_index)
       end
     end
   end
